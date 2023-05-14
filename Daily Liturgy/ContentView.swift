3d158cae00b29310d37050e322cb780b519ea9ed
Caleb Koch <kochcj1@gmail.com>
2023-05-14 06:35:24 -0400

    Initial Commit

---
+++ Daily Liturgy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daily Liturgy
+//
+//  Created by Caleb Koch on 5/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daily Liturgy/Daily_LiturgyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_LiturgyApp.swift
+//  Daily Liturgy
+//
+//  Created by Caleb Koch on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Daily_LiturgyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
