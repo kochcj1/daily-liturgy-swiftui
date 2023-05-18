e8030e169b219ac0cfeece5df15ae76779f2e2fb
Caleb Koch <kochcj1@gmail.com>
2023-05-18 19:21:39 -0400

    Comment for fix for silent mode bug

--- Daily Liturgy/DailyLiturgyApp.swift
+++ Daily Liturgy/DailyLiturgyApp.swift
@@ -13,6 +13,7 @@ struct DailyLiturgyApp: App {
     var body: some Scene {
         WindowGroup {
             SplashView().onAppear {
+                // So that audio will play even when the device is in silent mode:
                 do {
                     try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [])
                     try AVAudioSession.sharedInstance().setActive(true)
