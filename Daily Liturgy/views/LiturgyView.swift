a4270da8dff274b4987e17ca0cf07690ddf53ad5
Caleb Koch <kochcj1@gmail.com>
2023-05-18 19:25:34 -0400

    A better way to space items

--- Daily Liturgy/views/LiturgyView.swift
+++ Daily Liturgy/views/LiturgyView.swift
@@ -15,22 +15,18 @@ struct LiturgyView: View {
     }
     
     var body: some View {
-        VStack {
-            Card(title: "Opening Prayer", description: liturgy.openingPrayer!, icon: "icons8-pray-64").padding(getCardPadding())
-            Card(title: "Confession of Sin & Prayer for Grace", description: liturgy.confessionOfSinAndPrayerForGrace!, icon: "icons8-pray-64").padding(getCardPadding())
-            ReadingCard(title: "Old Testament Reading", passage: liturgy.oldTestamentReading!).padding(getCardPadding())
-            Card(title: "The Lord’s Prayer", description: liturgy.theLordsPrayer, icon: "icons8-pray-64").padding(getCardPadding())
-            ReadingCard(title: "New Testament Reading", passage: liturgy.newTestamentReading!).padding(getCardPadding())
-            Card(title: "Prayer of Adoration", description: liturgy.prayerOfAdoration!, icon: "icons8-pray-64").padding(getCardPadding())
-            ReadingCard(title: "Psalm Reading", passage: liturgy.psalmReading!).padding(getCardPadding())
-            Card(title: "Prayer of Consecration", description: liturgy.prayerOfConsecration!, icon: "icons8-pray-64").padding(getCardPadding())
-            Card(title: "Benediction", description: liturgy.benediction, icon: "icons8-gift-96").padding(getCardPadding())
+        VStack(spacing: 25) {
+            Card(title: "Opening Prayer", description: liturgy.openingPrayer!, icon: "icons8-pray-64")
+            Card(title: "Confession of Sin & Prayer for Grace", description: liturgy.confessionOfSinAndPrayerForGrace!, icon: "icons8-pray-64")
+            ReadingCard(title: "Old Testament Reading", passage: liturgy.oldTestamentReading!)
+            Card(title: "The Lord’s Prayer", description: liturgy.theLordsPrayer, icon: "icons8-pray-64")
+            ReadingCard(title: "New Testament Reading", passage: liturgy.newTestamentReading!)
+            Card(title: "Prayer of Adoration", description: liturgy.prayerOfAdoration!, icon: "icons8-pray-64")
+            ReadingCard(title: "Psalm Reading", passage: liturgy.psalmReading!)
+            Card(title: "Prayer of Consecration", description: liturgy.prayerOfConsecration!, icon: "icons8-pray-64")
+            Card(title: "Benediction", description: liturgy.benediction, icon: "icons8-gift-96")
             Spacer(minLength: 25)
         }.padding().padding()
-    }
-    
-    private func getCardPadding() -> EdgeInsets {
-        return EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
     }
 }
 
